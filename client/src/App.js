162c1bc7b876bb926cd1630526b6beeed5830868
Simply-divine <hardikvu0204@gmail.com>
2021-04-19 08:30:55 +0530

    chore: add public, private, restricted routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import { Provider as StoreProvider, useDispatch } from 'react-redux';
 import { Router } from 'react-router';
+import MainRouter from './routes/routes';
 import { history, store } from './store';
 
 const App = () => {
   return (
     <StoreProvider store={store}>
-      <Router history={history}></Router>
+      <Router history={history}>
+        <MainRouter></MainRouter>
+      </Router>
     </StoreProvider>
   );
 };
---
+++ client/src/routes/routes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, BrowserRouter } from 'react-router-dom';
+
+const MainRouter = () => (
+  <BrowserRouter>
+    <Switch>
+      {/* Restricted Routes */}
+
+      {/* Private Routes */}
+
+      {/* Public Routes */}
+    </Switch>
+  </BrowserRouter>
+);
+
+export default MainRouter;
