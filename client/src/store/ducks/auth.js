975adf6d7a87c9f5909e8ab485e545a9b0bcaf56
Simply-divine <hardikvu0204@gmail.com>
2021-04-21 19:43:11 +0530

    chore: add error and loading store states

--- client/src/store/ducks/auth.js
+++ client/src/store/ducks/auth.js
@@ -1,7 +1,8 @@
 // ? idea: https://github.com/erikras/ducks-modular-redux
 import { API, ApiActionCreator } from '../helpers';
+import { CLEAR_ALL_ERRORS } from './errors';
 
-// export const CHECK_AUTH = ApiActionCreator('auth/CHECK_AUTH');
+export const CHECK_AUTH = ApiActionCreator('auth/CHECK_AUTH');
 export const SIGNUP = ApiActionCreator('auth/SIGNUP');
 export const LOGIN = ApiActionCreator('auth/LOGIN');
 
@@ -35,10 +36,13 @@ export const signupUser = (formData) => ({
   },
   onRequest: SIGNUP.REQUEST,
   onSuccess: (dispatch, data) => {
+    dispatch({ type: CLEAR_ALL_ERRORS });
     dispatch({ type: SIGNUP.SUCCESS, payload: data });
   },
   onFailure: (dispatch, err) => {
     dispatch({ type: SIGNUP.FAILURE, payload: err });
+    dispatch({ type: SIGNUP.FAILURE, payload: err });
+    dispatch({ type: CLEAR_ALL_ERRORS });
   },
 });
 
@@ -51,20 +55,23 @@ export const loginUser = (formData) => ({
   },
   onRequest: LOGIN.REQUEST,
   onSuccess: (dispatch, data) => {
+    dispatch({ type: CLEAR_ALL_ERRORS });
     dispatch({ type: LOGIN.SUCCESS, payload: data });
   },
   onFailure: (dispatch, err) => {
     dispatch({ type: LOGIN.FAILURE, payload: err });
+    dispatch({ type: LOGIN.FAILURE, payload: err });
+    dispatch({ type: CLEAR_ALL_ERRORS });
   },
 });
 
-// export const checkAuth = () => ({
-//   type: API,
-//   payload: {
-//     method: 'GET',
-//     url: '/auth/check-auth',
-//   },
-//   onRequest: CHECK_AUTH.REQUEST,
-//   onSuccess: CHECK_AUTH.SUCCESS,
-//   onFailure: CHECK_AUTH.FAILURE,
-// });
+export const checkAuth = () => ({
+  type: API,
+  payload: {
+    method: 'GET',
+    url: '/auth/check-auth',
+  },
+  onRequest: CHECK_AUTH.REQUEST,
+  onSuccess: CHECK_AUTH.SUCCESS,
+  onFailure: CHECK_AUTH.FAILURE,
+});
